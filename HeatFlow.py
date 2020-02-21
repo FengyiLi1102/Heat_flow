4fb6fd1cf29432beb5c2a70bc0aeaa4ce53c5554
Fengyi Li <carl.yifeng.li@outlook.com>
2020-02-21 17:26:54 +0000

    Update of Parallel processing

--- HeatFlow.py
+++ HeatFlow.py
@@ -4,6 +4,7 @@ from getNeighbours import getNeighbours
 import matplotlib.pyplot as plt
 from simulate import run
 
+
 # Global parameters
 v = 21.8
 L = 0.1
@@ -11,13 +12,13 @@ t_Tot = L / v
 T_g = 573.15
 k_s = 16.3
 rho_s = 8030
-reco_step = 100
+reco_step = 10
 
 
-def main():
+def body():
     dh = 0.0001
     dt = 0.00001
-    time = 0.1
+    time = 0.001
     t_finish = int(time/dt)
     grid = PipeGrid(dh)
     T_Tot = []
@@ -34,11 +35,11 @@ def main():
                 T_Tot.append(grid)
         else:
             grid[:, 1] = T_g
-            t_start = t
             break
         
         run(grid, T_g, k_s, rho_s, T_Tot, dh, dt)
     
+    t_start = t
 
     for t in np.arange(t_start, t_finish):
         if t % reco_step == 0:
@@ -46,15 +47,14 @@ def main():
 
         run(grid, T_g, k_s, rho_s, T_Tot, dh, dt)
     
-    fig, ax = plt.subplots(111, dpi=300)
     t_list = np.arange(len(T_Tot)) * dt
     T_plot = []
     for T in T_Tot:
         T_plot.append(T[int(grid.shape[0]/2), grid.shape[1]-2])
-    ax.plot(t_list, T_plot, 'r-')
-    fig.savefig('image.png')
-    plt.close(fig)
+
+
+    return t_list, T_plot
 
 
 if __name__ == '__main__':
-    main()
+    body()
---
+++ parallelpro.py
@@ -0,0 +1,31 @@
+import multiprocessing as mp
+from HeatFlow import body
+import matplotlib.pyplot as plt
+import matplotlib.pylab as plb
+import gc
+
+paras = {
+    'figure.figsize': (10, 8),
+    'axes.labelsize': 'x-large',
+    'axes.titlesize': 'x-large',
+    'xtick.labelsize': 'x-large',
+    'ytick.labelsize': 'x-large',
+}
+plb.rcParams.update(paras)
+
+
+def main():
+
+    pool = mp.Pool(mp.cpu_count())
+    time, temp = pool.apply(body)
+    pool.close()
+
+    fig = plt.figure(dpi=300)
+    ax = fig.add_subplot(111)
+    ax.plot(time, temp,'r-')
+    plt.close(fig)
+    gc.collect()
+
+
+if __name__ == '__main__':
+    main()
